0dbe7cc5ace2a40d81ee84dcf40591a9c3dcf217
Hadelin de Ponteves <39771260+hadelin2p@users.noreply.github.com>
2019-03-24 17:07:45 +0100

    Update thompson_sampling.py

--- thompson_sampling.py
+++ thompson_sampling.py
@@ -1,5 +1,4 @@
-# Artificial Intelligence for Business
-# Maximizing the Revenues of an Online Retail Business with Thompson Sampling
+# AI for Sales & Advertizing - Sell like the Wolf of AI Street
 
 # Importing the libraries
 import numpy as np
@@ -10,8 +9,7 @@ import random
 N = 10000
 d = 9
 
-# Creating the simulation
-# conversion_rates = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+# Building the environment inside a simulation
 conversion_rates = [0.05,0.13,0.09,0.16,0.11,0.04,0.20,0.08,0.01]
 X = np.array(np.zeros([N,d]))
 for i in range(N):
