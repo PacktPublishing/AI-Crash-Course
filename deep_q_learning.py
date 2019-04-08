7c721540b2c6725d63dac0d93c113d232018112b
Hadelin de Ponteves <39771260+hadelin2p@users.noreply.github.com>
2019-04-08 14:04:15 +0200

    Update deep_q_learning.py

--- deep_q_learning.py
+++ deep_q_learning.py
@@ -57,7 +57,7 @@ class Dqn(object):
         self.last_reward = 0
     
     def select_action(self, state):
-        probs = F.softmax(self.model(Variable(state, volatile = True))*100)
+        probs = F.softmax(self.model(Variable(state)) * 100)
         action = probs.multinomial()
         return action.data[0,0]
     
