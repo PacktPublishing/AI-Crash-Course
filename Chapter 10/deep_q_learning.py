cf5b9b3cad0b5498d8dd1afa19fbde3d99b4a16c
Hadelin de Ponteves <Hadelin@Hadelins-iMac.local>
2019-06-03 12:34:37 +0400

    Initial Commit

---
+++ Chapter 10/deep_q_learning.py
@@ -0,0 +1,98 @@
+# AI for Autonomous Vehicles - Build a Self-Driving Car
+
+# Importing the libraries
+
+import os
+import random
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+# Creating the architecture of the Neural Network
+
+class Network(nn.Module):
+    
+    def __init__(self, input_size, nb_action):
+        super(Network, self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size, 30)
+        self.fc2 = nn.Linear(30, nb_action)
+    
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        q_values = self.fc2(x)
+        return q_values
+
+# Implementing Experience Replay
+
+class ReplayMemory(object):
+    
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+    
+    def push(self, event):
+        self.memory.append(event)
+        if len(self.memory) > self.capacity:
+            del self.memory[0]
+    
+    def sample(self, batch_size):
+        samples = zip(*random.sample(self.memory, batch_size))
+        return map(lambda x: Variable(torch.cat(x, 0)), samples)
+
+# Implementing Deep Q-Learning
+
+class Dqn(object):
+    
+    def __init__(self, input_size, nb_action, gamma):
+        self.gamma = gamma
+        self.model = Network(input_size, nb_action)
+        self.memory = ReplayMemory(capacity = 100000)
+        self.optimizer = optim.Adam(params = self.model.parameters())
+        self.last_state = torch.Tensor(input_size).unsqueeze(0)
+        self.last_action = 0
+        self.last_reward = 0
+    
+    def select_action(self, state):
+        probs = F.softmax(self.model(Variable(state))*100)
+        action = probs.multinomial()
+        return action.data[0,0]
+    
+    def learn(self, batch_states, batch_actions, batch_rewards, batch_next_states):
+        batch_outputs = self.model(batch_states).gather(1, batch_actions.unsqueeze(1)).squeeze(1)
+        batch_next_outputs = self.model(batch_next_states).detach().max(1)[0]
+        batch_targets = batch_rewards + self.gamma * batch_next_outputs
+        td_loss = F.smooth_l1_loss(batch_outputs, batch_targets)
+        self.optimizer.zero_grad()
+        td_loss.backward()
+        self.optimizer.step()
+    
+    def update(self, new_state, new_reward):
+        new_state = torch.Tensor(new_state).float().unsqueeze(0)
+        self.memory.push((self.last_state, torch.LongTensor([int(self.last_action)]), torch.Tensor([self.last_reward]), new_state))
+        new_action = self.select_action(new_state)
+        if len(self.memory.memory) > 100:
+            batch_states, batch_actions, batch_rewards, batch_next_states = self.memory.sample(100)
+            self.learn(batch_states, batch_actions, batch_rewards, batch_next_states)
+        self.last_state = new_state
+        self.last_action = new_action
+        self.last_reward = new_reward
+        return new_action
+    
+    def save(self):
+        torch.save({'state_dict': self.model.state_dict(),
+                    'optimizer' : self.optimizer.state_dict(),
+                   }, 'last_brain.pth')
+    
+    def load(self):
+        if os.path.isfile('last_brain.pth'):
+            print("=> loading checkpoint... ")
+            checkpoint = torch.load('last_brain.pth')
+            self.model.load_state_dict(checkpoint['state_dict'])
+            self.optimizer.load_state_dict(checkpoint['optimizer'])
+            print("done !")
+        else:
+            print("no checkpoint found...")
---
+++ Chapter 8/q_learning.py
@@ -0,0 +1,81 @@
+# AI for Logistics - Robots in a warehouse
+
+# Importing the libraries
+import numpy as np
+
+# Setting the parameters gamma and alpha for the Q-Learning
+gamma = 0.75
+alpha = 0.9
+
+# PART 1 - BUILDING THE ENVIRONMENT
+
+# Defining the states
+location_to_state = {'A': 0,
+                     'B': 1,
+                     'C': 2,
+                     'D': 3,
+                     'E': 4,
+                     'F': 5,
+                     'G': 6,
+                     'H': 7,
+                     'I': 8,
+                     'J': 9,
+                     'K': 10,
+                     'L': 11}
+
+# Defining the actions
+actions = [0,1,2,3,4,5,6,7,8,9,10,11]
+
+# Defining the rewards
+R = np.array([[0,1,0,0,0,0,0,0,0,0,0,0],
+              [1,0,1,0,0,1,0,0,0,0,0,0],
+              [0,1,0,0,0,0,1,0,0,0,0,0],
+              [0,0,0,0,0,0,0,1,0,0,0,0],
+              [0,0,0,0,0,0,0,0,1,0,0,0],
+              [0,1,0,0,0,0,0,0,0,1,0,0],
+              [0,0,1,0,0,0,1,1,0,0,0,0],
+              [0,0,0,1,0,0,1,0,0,0,0,1],
+              [0,0,0,0,1,0,0,0,0,1,0,0],
+              [0,0,0,0,0,1,0,0,1,0,1,0],
+              [0,0,0,0,0,0,0,0,0,1,0,1],
+              [0,0,0,0,0,0,0,1,0,0,1,0]])
+
+# PART 2 - BUILDING THE AI SOLUTION WITH Q-LEARNING
+
+# Making a mapping from the states to the locations
+state_to_location = {state: location for location, state in location_to_state.items()}
+
+# Making a function that returns the shortest route from a starting to ending location
+def route(starting_location, ending_location):
+    R_new = np.copy(R)
+    ending_state = location_to_state[ending_location]
+    R_new[ending_state, ending_state] = 1000
+    Q = np.array(np.zeros([12,12]))
+    for i in range(1000):
+        current_state = np.random.randint(0,12)
+        playable_actions = []
+        for j in range(12):
+            if R_new[current_state, j] > 0:
+                playable_actions.append(j)
+        next_state = np.random.choice(playable_actions)
+        TD = R_new[current_state, next_state] + gamma * Q[next_state, np.argmax(Q[next_state,])] - Q[current_state, next_state]
+        Q[current_state, next_state] = Q[current_state, next_state] + alpha * TD
+    route = [starting_location]
+    next_location = starting_location
+    while (next_location != ending_location):
+        starting_state = location_to_state[starting_location]
+        next_state = np.argmax(Q[starting_state,])
+        next_location = state_to_location[next_state]
+        route.append(next_location)
+        starting_location = next_location
+    return route
+
+# PART 3 - GOING INTO PRODUCTION
+
+# Making the final function that returns the optimal route
+def best_route(starting_location, intermediary_location, ending_location):
+    return route(starting_location, intermediary_location) + route(intermediary_location, ending_location)[1:]
+
+# Printing the final route
+print('Route:')
+best_route('E', 'K', 'G')
