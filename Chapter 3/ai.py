186811d75886832683243e61bb753a1b22ff1275
Hadelin de Ponteves <39771260+hadelin2p@users.noreply.github.com>
2019-06-21 02:38:32 +0400

    Update ai.py

--- Chapter 3/ai.py
+++ Chapter 3/ai.py
@@ -1,50 +1,40 @@
-# Predicting House Prices
+# Thompson Sampling for Slot Machines
 
 # Importing the libraries
-import pandas as pd
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-import keras
-from keras.layers import Dense, Dropout
-from keras.models import Sequential
-from keras.optimizers import Adam
 
-# Importing the dataset
-dataset = pd.read_csv('kc_house_data.csv')
+# Setting conversion rates and the number of samples
+conversionRates = [0.15, 0.04, 0.13, 0.11, 0.05]
+N = 10000
+d = len(conversionRates)
 
-# Getting separately the features and the targets
-X = dataset.iloc[:, 3:].values
-X = X[:, np.r_[0:13,14:18]]
-y = dataset.iloc[:, 2].values
+# Creating the dataset
+X = np.zeros((N, d))
+for i in range(N):
+    for j in range(d):
+        if np.random.rand() < conversionRates[j]:
+            X[i][j] = 1
 
-# Splitting the dataset into a training set and a test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+# Making arrays to count our losses and wins
+nPosReward = np.zeros(d)
+nNegReward = np.zeros(d)
 
-# Scaling the features
-xscaler = MinMaxScaler(feature_range = (0,1))
-X_train = xscaler.fit_transform(X_train)
-X_test = xscaler.transform(X_test)
+# Taking our best slot machine through beta distibution and updating its losses and wins
+for i in range(N):
+    selected = 0
+    maxRandom = 0
+    for j in range(d):
+        randomBeta = np.random.beta(nPosReward[j] + 1, nNegReward[j] + 1)
+        if randomBeta > maxRandom:
+            maxRandom = randomBeta
+            selected = j
+    if X[i][selected] == 1:
+        nPosReward[selected] += 1
+    else:
+        nNegReward[selected] += 1
 
-# Scaling the target
-yscaler = MinMaxScaler(feature_range = (0,1))
-y_train = yscaler.fit_transform(y_train.reshape(-1,1))
-y_test = yscaler.transform(y_test.reshape(-1,1))
-
-# Building the Artificial Neural Network
-model = Sequential()
-model.add(Dense(units = 64, kernel_initializer = 'uniform', activation = 'relu', input_dim = 17))
-model.add(Dense(units = 16, kernel_initializer = 'uniform', activation = 'relu'))
-model.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'relu'))
-model.compile(optimizer = Adam(lr = 0.001), loss = 'mse', metrics = ['mean_absolute_error'])
-
-# Training the Artificial Neural Network
-model.fit(X_train, y_train, batch_size = 32, epochs = 100, validation_data = (X_test, y_test))
-
-# Making predictions on the test set while reversing the scaling
-y_test = yscaler.inverse_transform(y_test)
-prediction = yscaler.inverse_transform(model.predict(X_test))
-
-# Computing the accuracy
-accuracy = 1 - abs(prediction - y_test)/y_test * 100
-print(np.mean(accuracy))
+# Showing which slot machine is considered the best
+nSelected = nPosReward + nNegReward 
+for i in range(d):
+    print('Machine number ' + str(i + 1) + ' was selected ' + str(nSelected[i]) + ' times')
+print('Conclusion: Best machine is machine number ' + str(np.argmax(nSelected) + 1))
