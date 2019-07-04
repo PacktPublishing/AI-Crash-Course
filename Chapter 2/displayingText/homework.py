f52aa0c398b66b15121224044293ded3adf2979a
Jan Warchocki <43212272+Jaswar@users.noreply.github.com>
2019-07-04 13:57:52 +0200

    Add files via upload

---
+++ Chapter 2/displayingText/homework.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  3 16:22:30 2019
+
+@author: janwa
+"""
+
+print('First line\nSecond line')    # We only have to write '\n' in the middle of the
+                                    # text we want to display. This will separate it into 
+                                    # two consecutive lines.
