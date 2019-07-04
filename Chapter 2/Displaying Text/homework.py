2c1d53644627df9bbf2bc90ec4a850b482ae85fd
Jan Warchocki <43212272+Jaswar@users.noreply.github.com>
2019-07-04 16:18:08 +0200

    Update homework.py

--- Chapter 2/Displaying Text/homework.py
+++ Chapter 2/Displaying Text/homework.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jul  3 16:22:30 2019
-
-@author: janwa
-"""
+#Exercise for Displaying Text: Homework Solution
 
 print('First line\nSecond line')    # We only have to write '\n' in the middle of the
                                     # text we want to display. This will separate it into 
