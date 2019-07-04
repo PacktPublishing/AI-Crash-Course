30ac8f561da3fe3ec57e1a56b8068173d765c511
Jan Warchocki <43212272+Jaswar@users.noreply.github.com>
2019-07-04 16:20:17 +0200

    Update and rename Chapter 2/forAndWhileLoops/homework.py to Chapter 2/For and While Loops/homework.py

--- Chapter 2/For and While Loops/homework.py
+++ Chapter 2/For and While Loops/homework.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jul  3 16:57:09 2019
-
-@author: janwa
-"""
+#Exercise for For and While Loops: Homework Solution
 
 n = 5                       # the number factorial of which we are searching for
 
