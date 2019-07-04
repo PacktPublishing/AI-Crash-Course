485f515005d7d33c58fe44743654c90203970393
Jan Warchocki <43212272+Jaswar@users.noreply.github.com>
2019-07-04 13:58:48 +0200

    Add files via upload

---
+++ Chapter 2/variables/homework.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  3 17:37:16 2019
+
+@author: janwa
+"""
+
+a = 3               # we will raise this number to a power
+b = 4               # this is the power to which we will raise "a"
+
+power = pow(a, b)   # pow() function let's us raise "a" to the "b" power
+
+print(power)        # we display the calculated power
