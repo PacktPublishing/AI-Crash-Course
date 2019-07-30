f9bc2feb70b381fd3fa2e44941f422a550742fe3
Hadelin de Ponteves <39771260+hadelin2p@users.noreply.github.com>
2019-07-30 21:54:37 +0200

    Update homework.py

--- Chapter 1/For and While Loops/homework.py
+++ Chapter 1/For and While Loops/homework.py
@@ -1,14 +1,14 @@
-#Exercise for For and While Loops: Homework Solution
+# Exercise for For and While Loops: Homework Solution
 
 n = 5                       # the number factorial of which we are searching for
 
-#for loop approach
+# for loop approach
 factorial = 1               # this will be out factorial, we give it value 1
 for i in range(1, n + 1):   # we iterate through every positive integer lower or equal to n, remember, upper bounds are excluded in Python
     factorial *= i          # we multiply factorial by i, this is why we initialized factorial to 1, if we set it to 0, then the product would be equal to 0
 print(factorial)            # we display the product, factorial
 
-#while loop approach
+# while loop approach
 factorial = 1               # this will be out factorial, we give it value 1
 i = 1                       # we create a variable i, which as previously, will count the number of iterations
 while i <= n:               # initializing "while" loop, that will work as long as i is less or equal to n
