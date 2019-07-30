8d34f67a4cd5532d7c157ce997aa419eae1a91e6
Hadelin de Ponteves <39771260+hadelin2p@users.noreply.github.com>
2019-07-30 21:53:01 +0200

    Update forAndWhileLoops.py

--- Chapter 1/For and While Loops/forAndWhileLoops.py
+++ Chapter 1/For and While Loops/forAndWhileLoops.py
@@ -1,22 +1,19 @@
-#Introduction to For and While Loops
+# Introduction to For and While Loops
 
 for i in range(1, 20):
     print(i)
 
-
 L3 = [3,4,1,6,7,5]
 for element in L3:
     print(element)
-    
-    
+
 stop = False
 i = 0
-while stop == False:  #Alternatively it can be "while not stop:"
+while stop == False:  # Alternatively it can be "while not stop:"
     i += 1
     print(i)
     if i >= 19:
         stop = True
-
 
 L4 = [[2, 9, -5], [-1, 0, 4], [3, 1, 2]]
 for row in L4:
