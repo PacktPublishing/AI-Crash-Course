201ab26f2ef582ab88fa6b76957eaac9319646f1
Jan Warchocki <43212272+Jaswar@users.noreply.github.com>
2019-08-09 15:17:38 +0200

    Update DQN.py

--- Chapter 10/DQN.py
+++ Chapter 10/DQN.py
@@ -1,33 +1,43 @@
-# AI for Snake using Deep Q-Learning and Convolutional Neural Networks: DQN memory
+# Snake: Deep Convolutional Q-Learning - Experience Replay Memory file
+# AI for Business - Minimize cost with Deep Q-Learning
+# Implementing Deep Q-Learning with Experience Replay
 
+# Importing the libraries
 import numpy as np
+
+# IMPLEMENTING DEEP Q-LEARNING WITH EXPERIENCE REPLAY
 
 class Dqn(object):
     
-    def __init__(self, maxMemory, discount = 0.9):
-        self.maxMemory = maxMemory
-        self.discount = discount
+    # INTRODUCING AND INITIALIZING ALL THE PARAMETERS AND VARIABLES OF THE DQN
+    def __init__(self, max_memory = 100, discount = 0.9):
         self.memory = list()
-        
-    def remember(self, transition, gameOver):
-        self.memory.append([transition, gameOver])
-        if len(self.memory) > self.maxMemory:
+        self.max_memory = max_memory
+        self.discount = discount
+
+    # MAKING A METHOD THAT BUILDS THE MEMORY IN EXPERIENCE REPLAY
+    def remember(self, transition, game_over):
+        self.memory.append([transition, game_over])
+        if len(self.memory) > self.max_memory:
             del self.memory[0]
-    
-    def getBatch(self, model, batchSize):
-        lenMemory = len(self.memory)
-        numOutputs = model.output_shape[-1]
-        inputs = np.zeros((min(lenMemory, batchSize), self.memory[0][0][0].shape[1],self.memory[0][0][0].shape[2],self.memory[0][0][0].shape[3]))
-        targets = np.zeros((min(lenMemory, batchSize), numOutputs))
-        for i, idx in enumerate(np.random.randint(0, lenMemory, size = min(lenMemory, batchSize))):
-            currentState, action, reward, nextState = self.memory[idx][0]
-            gameOver = self.memory[idx][1]
-            inputs[i] = currentState
-            targets[i] = model.predict(currentState)[0]
-            Qsa = np.max(model.predict(nextState)[0])
-            if gameOver:
+
+    # MAKING A METHOD THAT BUILDS TWO BATCHES OF INPUTS AND TARGETS BY EXTRACTING TRANSITIONS FROM THE MEMORY
+    def get_batch(self, model, batch_size = 10):
+        len_memory = len(self.memory)
+        num_outputs = model.output_shape[-1]
+        inputs = np.zeros((min(len_memory, batch_size), self.memory[0][0][0].shape[1],self.memory[0][0][0].shape[2],self.memory[0][0][0].shape[3]))
+        targets = np.zeros((min(len_memory, batch_size), num_outputs))
+        for i, idx in enumerate(np.random.randint(0, len_memory, size = min(len_memory, batch_size))):
+            current_state, action, reward, next_state = self.memory[idx][0]
+            game_over = self.memory[idx][1]
+            inputs[i] = current_state
+            targets[i] = model.predict(current_state)[0]
+            Q_sa = np.max(model.predict(next_state)[0])
+            if game_over:
                 targets[i, action] = reward
             else:
-                targets[i, action] = reward + self.discount * Qsa
-            
+                targets[i, action] = reward + self.discount * Q_sa
         return inputs, targets
+
+
+   
