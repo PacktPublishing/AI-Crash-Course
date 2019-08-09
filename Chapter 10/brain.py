640228219fd8176b9a660a68622eb9d8612cb260
Jan Warchocki <43212272+Jaswar@users.noreply.github.com>
2019-08-09 21:49:01 +0200

    Update brain.py

--- Chapter 10/brain.py
+++ Chapter 10/brain.py
@@ -1,10 +1,12 @@
 # AI for Snake using Deep Q-Learning and Convolutional Neural Networks: the Snake's brain
 
+# Importing the libraries
 import keras
 from keras.models import Sequential, load_model
 from keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, Flatten
 from keras.optimizers import Adam
 
+# Creating the Brain class
 class Brain():
     
     def __init__(self, iS = (100,100,3), lr = 0.0005):
@@ -14,6 +16,7 @@ class Brain():
         self.numOutputs = 4
         self.model = Sequential() 
         
+        # Adding layers to the model
         self.model.add(Conv2D(32, (3,3), activation = 'relu', input_shape = self.inputShape))
         
         self.model.add(MaxPooling2D((2,2)))
@@ -26,8 +29,10 @@ class Brain():
         
         self.model.add(Dense(units = self.numOutputs))
         
+        # Compiling the model
         self.model.compile(loss = 'mean_squared_error', optimizer = Adam(lr = self.learningRate))
     
+    # Making a function that will load a model from a file
     def loadModel(self, filepath):
         self.model = load_model(filepath)
         return self.model
