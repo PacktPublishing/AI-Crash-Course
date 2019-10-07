dcda5af71913733f8d998a32ac101270daad0576
Jan Warchocki <43212272+Jaswar@users.noreply.github.com>
2019-10-07 17:17:35 +0200

    Delete homework.py

--- Chapter 1/Displaying Text/homework.py
+++
@@ -1,5 +0,0 @@
-# Exercise for Displaying Text: Homework Solution
-
-print('First line\nSecond line')    # We only have to write '\n' in the middle of the
-                                    # text we want to display. This will separate it into 
-                                    # two consecutive lines.
