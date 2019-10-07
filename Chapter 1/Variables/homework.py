77b42cce966f36f990f44b343f06711bcc97c98a
Jan Warchocki <43212272+Jaswar@users.noreply.github.com>
2019-10-07 17:18:52 +0200

    Delete homework.py

--- Chapter 1/Variables/homework.py
+++
@@ -1,8 +0,0 @@
-# Exercise for Variables: Homework Solution
-
-a = 3               # we will raise this number to a power
-b = 4               # this is the power to which we will raise "a"
-
-power = pow(a, b)   # pow() function let's us raise "a" to the "b" power
-
-print(power)        # we display the calculated power
